43b1c61cfd67e6c8c72e3ddb5f5fa7909afa8dbe
Irving Mariscales <mariscales.irving@gmail.com>
2019-12-22 21:40:17 -0700

    cover 100%

--- src/components/landing/Cover.js
+++ src/components/landing/Cover.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react'
 import '../styles/landing.css'
+import arrow from '../../assets/arrow.svg'
 
 export default class Cover extends Component {
     render() {
         return (
-            <div id="cover">
-                Hey! <br/>
-                I’m Irving Mariscales, <br/>
-                a software developer with a taste for design and interactivity
+            <div id="cover" className="lateral-margin">
+                <p>
+                    Hey! <br/>
+                    I’m Irving Mariscales, <br/>
+                    a software developer with a taste for design and interactivity
+                </p>
+
+                <label id="arrow">
+                    <img src={arrow} className="up"/>
+                </label>
             </div>
         )
     }
