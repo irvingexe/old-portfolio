c1d1185f9cc46347e9600b37a71a5b923d55351f
Irving Mariscales <mariscales.irving@gmail.com>
2019-12-24 12:38:21 -0700

    responsive navbar

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import React from 'react';
 import './App.css';
 import Background from './components/landing/Background';
 import Cover from './components/landing/Cover';
-import Header from './components/fixed/Header';
+import Nav from './components/fixed/Nav';
 
 
 function App() {
   return (
     <div className="App">
       <Background/>
-      <Header/>
+      <Nav/>
       <Cover/>
-      <div className="asdf"></div>
+      <Cover/>
     </div>
   );
 }
---
+++ src/components/fixed/Nav.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+import Logo from './Logo'
+import '../styles/nav.css'
+
+export default class Nav extends Component {
+
+    constructor(props){
+        super(props)
+        this.state = {menuState: false};
+    }
+
+    menuState = () => {this.setState((state, props) => ({menuState:!state.menuState}))}
+
+    render() {
+        console.log(this.state.menuState)
+        return (
+            <nav id="nav" className="fixed font-xs">
+                <label className="App-logo">
+                    <Logo/>
+                </label>
+                <ul id="menu-items"   className={"center" + (this.state.menuState? " active" :"")}>
+                    <li><a href="#">I do</a></li>
+                    <li><a href="#">Work</a></li>
+                    <li><a href="#">Who</a></li>
+                    <li id="contact"><a href="#">Contact</a></li>
+                </ul>
+                <label id="toggle" onClick={this.menuState}>
+                    <ul className={"buns" + (this.state.menuState? " active" :"")}>
+                        <li className="bun"></li>
+                        <li className="bun"></li>
+                    </ul>
+                </label>
+            </nav>
+        )
+    }
+}
