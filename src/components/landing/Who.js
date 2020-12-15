d0b4d47b70e8841cb0a6a3377ad55c607ba6b6fe
Irving Mariscales <mariscales.irving@gmail.com>
2020-12-15 13:51:03 -0700

    minor fixes

--- src/components/landing/Who.js
+++ src/components/landing/Who.js
@@ -11,13 +11,13 @@ export default function Who() {
             <div>
                 <h1 className="font-m">In a few words</h1>
                 <p className="font-s">
-                    I like to play around with different tecnologies and 
-                    I see myself every day more involved into interactivity. 
+                    I am a software developer who enjoy creating beautiful design and good experiences.
                     Simply passionated with create pixel perfect experiences.
                 </p>
                 <p className="font-s">
-                    I am a software developer who enjoy creating beautiful design and good experiences.
-                    I’m experienced working as a full-stack but I’m majorly involved into front-end developement. 
+                    I’m majorly involved into front-end developement
+                    and I like to play around with different tecnologies. 
+                    I see myself every day more involved into interactivity.
                 </p>
             </div>
             <div className="bg-title font-xxl bold center unselectable"><h1>Who</h1></div>
