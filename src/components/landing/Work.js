cb17e961a006d10fe2464d62a347a011c48e29ee
Irving Mariscales <mariscales.irving@gmail.com>
2020-12-27 17:58:43 -0700

    mockups rotation

--- src/components/landing/Work.js
+++ src/components/landing/Work.js
@@ -1,10 +1,15 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import Context from "../../store/context";
 import Mockup from "./Mockup";
 import infoProjects from "../projects.json";
 
 export default function Work() {
   const { state, actions } = useContext(Context);
+  const [axis, setAxis] = useState({
+    phone: { x: -20, y: -85 },
+    desk: { x: -10, y: -90 },
+  });
+  const [transition, setTransition] = useState("all 0.2s ease");
 
   const cursorProject = () => {
     actions({
@@ -28,6 +33,23 @@ export default function Work() {
       },
     });
   };
+  const transform = (e) => {
+    let xAxisP = e.screenX / 2 / 30 - 30;
+    let yAxisP = -e.screenY / 2 / 30 - 75;
+    let xAxisD = e.screenX / 2 / 30 - 20;
+    let yAxisD = -e.screenY / 2 / 30 - 80;
+    setAxis({
+      phone: { x: xAxisP, y: yAxisP },
+      desk: { x: xAxisD, y: yAxisD },
+    });
+  };
+  const stopTransform = () => {
+    setAxis({ phone: { x: -20, y: -85 }, desk: { x: -10, y: -90 } });
+    setTransition("all 0.5s ease");
+  };
+  const fastTransition = () => {
+    setTransition("all 0.2s ease");
+  };
 
   const projects = [];
 
@@ -46,6 +68,16 @@ export default function Work() {
           onMouseLeave={resetCursor}
         >
           <Mockup
+            style={{
+              phone: {
+                transform: `rotateX(${axis.phone.y}deg) rotateZ(${axis.phone.x}deg) scale3d(0.7, 0.7, 0.7)`,
+                transition: transition,
+              },
+              desk: {
+                transform: `rotateX(${axis.desk.y}deg) rotateZ(${axis.desk.x}deg) scale3d(0.7, 0.7, 0.7)`,
+                transition: transition,
+              },
+            }}
             device={infoProjects[i].device}
             img1={"../../assets/projects/" + i + "/0.png"}
             img2={"../../assets/projects/" + i + "/-1.png"}
@@ -58,7 +90,12 @@ export default function Work() {
   }
 
   return (
-    <div id="work">
+    <div
+      id="work"
+      onMouseMove={transform}
+      onMouseLeave={stopTransform}
+      onMouseEnter={fastTransition}
+    >
       {projects}
       <div className="bg-title font-xxl bold center unselectable">
         <h1>Works</h1>
