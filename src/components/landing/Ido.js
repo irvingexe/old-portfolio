29694007415b4368f4622c9240ed181f8a882970
Irving Mariscales <mariscales.irving@gmail.com>
2021-01-08 19:37:15 -0700

    ido titles and get info from json

--- src/components/landing/Ido.js
+++ src/components/landing/Ido.js
@@ -4,19 +4,48 @@ import "../../styles/landing.css";
 
 export default function Ido() {
   let blocks = [];
-  for (let i = 0; i < 18; i++) {
+  let key = 0;
+  ido.forEach((e) => {
     blocks.push(
-      <div key={i} href="#" className="box">
-        <div className="border"></div>
+      <div key={key++} href="#" className="box">
+        <div className="border center title">
+          <div className="content center">
+            <div className="center font-s">{e.description}</div>
+            <div className="center">
+              {e.name}
+              <br />
+              {e.name2}
+            </div>
+          </div>
+        </div>
         <div className="top-left"></div>
         <div className="top-right"></div>
         <div className="bottom-left"></div>
         <div className="bottom-right"></div>
       </div>
     );
-  }
+
+    e.tecnologies.forEach((e2) => {
+      blocks.push(
+        <div key={key++} href="#" className="box">
+          <div className="border center">
+            <div className="content center">
+              <div className="center">
+                <img alt="" src={e2.img} />
+              </div>
+            </div>
+          </div>
+          <div className="top-left"></div>
+          <div className="top-right"></div>
+          <div className="bottom-left"></div>
+          <div className="bottom-right"></div>
+        </div>
+      );
+    });
+  });
+
   return (
-    <div id="ido" className="center">
+    <div id="ido" className="center scrollOut link" data-section="ido">
       <div className="container">
         <div className="footer">{blocks}</div>
       </div>
