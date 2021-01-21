9e7dd757904ed520438d23ea98eb1c028083a958
Irving Mariscales <mariscales.irving@gmail.com>
2021-01-21 16:32:28 -0700

    who redesigned

--- src/components/landing/Who.js
+++ src/components/landing/Who.js
@@ -1,5 +1,6 @@
 import React, { useContext } from "react";
 import Context from "../../store/context";
+import me from "../../assets/me.svg";
 
 export default function Who() {
   const { state, actions } = useContext(Context);
@@ -17,24 +18,24 @@ export default function Who() {
   };
 
   return (
-    <div
-      id="who"
-      className="center scrollOut"
-      data-section="who"
-      onMouseEnter={cursorImage}
-      onMouseLeave={resetCursor}
-    >
+    <div id="who" className="center scrollOut" data-section="who">
       <div>
-        <h1 className="font-m">In a few words</h1>
-        <p className="font-s">
-          I am a software developer who enjoy creating beautiful design and good
-          experiences. Simply passionated with create pixel perfect experiences.
-          <br />
-          <br />
-          I’m majorly involved into front-end developement and I like to play
-          around with different tecnologies. I see myself every day more
-          involved into interactivity.
-        </p>
+        <div>
+          <h1 className="font-m">In a few words</h1>
+          <p className="font-s">
+            I am a software developer who enjoy creating beautiful design and
+            good experiences. Simply passionated with create pixel perfect
+            experiences.
+            <br />
+            <br />
+            I’m majorly involved into front-end developement and I like to play
+            around with different tecnologies. I see myself every day more
+            involved into interactivity.
+          </p>
+        </div>
+        <div className="center">
+          <img src={me} />
+        </div>
       </div>
     </div>
   );
