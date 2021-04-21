0a2616bcd22a8477ed772a103c826de9097289c2
Irving Mariscales <mariscales.irving@gmail.com>
2021-04-21 16:07:18 -0600

    prepare for dedploy

--- src/components/landing/Landing.js
+++ src/components/landing/Landing.js
@@ -172,7 +172,6 @@ export default function Landing() {
         state.project.isOpened ? "project-open" : ""
       }`}
     >
-      <div>{window.mobileAndTabletCheck() ? "mobile" : "desktop"}</div>
       <Cover />
       <Work />
       <Ido />
